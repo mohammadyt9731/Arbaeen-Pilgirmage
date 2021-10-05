e3af91b23a2ac201c14a1417175ae0478edff3ab
Mohammad Yazdi <mohammadyt1379@gmail.com>
2021-10-06 01:16:07 +0330

    config NarrativesFragment and optimize fragment_narratives.xml

---
+++ app/src/main/java/com/ttp/ziaratarbaeen/classes/NarrativeAdapter.java
@@ -0,0 +1,74 @@
+package com.ttp.ziaratarbaeen.classes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ttp.ziaratarbaeen.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class NarrativeAdapter extends RecyclerView.Adapter<NarrativeAdapter.ViewHolder> {
+
+    ArrayList<String> narrativeTitleList;
+    ArrayList<String> narrativeTextList;
+
+
+    public NarrativeAdapter(Context context) {
+
+        narrativeTitleList = new ArrayList<>();
+        narrativeTextList = new ArrayList<>();
+
+        narrativeTitleList.addAll(Arrays.asList(context.getResources().getStringArray(R.array.narratives_title)));
+        narrativeTextList.addAll(Arrays.asList(context.getResources().getStringArray(R.array.narratives_text)));
+
+
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        return new ViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_narrative, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NarrativeAdapter.ViewHolder holder, int position) {
+
+        holder.tvTitle.setText(narrativeTitleList.get(position));
+        holder.tvText.setText(narrativeTextList.get(position));
+
+        if (holder.tvTitle.getText().length() == 0)
+            holder.tvTitle.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return narrativeTitleList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        TextView tvTitle;
+        TextView tvText;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.tv_narrative_title);
+            tvText = itemView.findViewById(R.id.tv_narrative_text);
+
+        }
+    }
+}
--- app/src/main/java/com/ttp/ziaratarbaeen/fragments/NarrativesFragment.java
+++ app/src/main/java/com/ttp/ziaratarbaeen/fragments/NarrativesFragment.java
@@ -4,17 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ttp.ziaratarbaeen.R;
 import com.ttp.ziaratarbaeen.classes.MyConstants;
 import com.ttp.ziaratarbaeen.classes.MyTapsell;
+import com.ttp.ziaratarbaeen.classes.NarrativeAdapter;
+
+import ir.tapsell.plus.TapsellPlusBannerType;
 
 public class NarrativesFragment extends Fragment {
 
+    RelativeLayout rlAdvertising;
+    RecyclerView rvNarratives;
 
     @Nullable
     @Override
@@ -27,8 +35,33 @@ public class NarrativesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        ViewGroup rlAdvertising = view.findViewById(R.id.rl_advertising);
-        MyTapsell.showNativeAD(getActivity(),MyConstants.NATIVE_STANDARD_AD_ID,rlAdvertising);
+        findViews(view);
+        setUpList();
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        MyTapsell.showStandardBanner(getActivity(), MyConstants.STANDARD_1_AD_ID, rlAdvertising, TapsellPlusBannerType.BANNER_320x50);
+    }
+
+    private void findViews(View view) {
+
+        rlAdvertising = view.findViewById(R.id.rl_advertising);
+        rvNarratives = view.findViewById(R.id.rv_narrative);
+
+    }
+
+    private void setUpList() {
+
+        NarrativeAdapter narrativeAdapter = new NarrativeAdapter(getActivity());
+
+        rvNarratives.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvNarratives.setAdapter(narrativeAdapter);
+
 
     }
 }
